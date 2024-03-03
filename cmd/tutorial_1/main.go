4be5b97127863136c866f9143edce62a09839f1a
Logan Cole <emtlogancole@gmail.com>
2024-03-03 16:30:56 -0600

    lesson one

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,6 +1,26 @@
 package main
 import "fmt"
+import "unicode/utf8"
 
 func main(){
-	fmt.Println("Hello World")
+	var intNum int16 = 32767
+	fmt.Println(intNum)
+
+	var floatNum float64 = 12345678.9
+	fmt.Println(floatNum)
+
+	var intNum1 int = 5
+	var intNum2 = 4
+	fmt.Println(intNum1/intNum2)
+
+	var myString string = "Hello World"
+	fmt.Println(len(myString))
+
+	fmt.Println(utf8.RuneCountInString("x"))
+
+	myVar := "words"
+	fmt.Println(myVar)
+
+	const myConst float64 = 3.14159265
+
 }
